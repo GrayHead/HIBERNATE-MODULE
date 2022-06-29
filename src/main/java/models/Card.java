5f73f88a47cd4e22e1b61bb4a935068bde7f7751
GrayHead <ss.zhuravlov@gmail.com>
2022-06-29 23:55:22 +0300

    25 one to many bidir

--- src/main/java/models/Card.java
+++ src/main/java/models/Card.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -19,6 +16,11 @@ public class Card implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String number;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_card", joinColumns = @JoinColumn(name = "card_id"), inverseJoinColumns = @JoinColumn(name = "user_id")
+
+    )
+    private User user;
 
     public Card(String number) {
         this.number = number;
